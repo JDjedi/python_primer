4a777db86626a9a5a9b5d20b32b1c9eeb102a0d5
JDjedi <jcdiaz1201@gmail.com>
2019-01-04 16:10:53 -0600

    Finished health.py script

--- PythonBible/health.py
+++ PythonBible/health.py
@@ -1,9 +1,10 @@
 import random 
 
 health = 50
-print(health)
+# difficulty slider: 1 = easy, 2 = medium, 3 = hard
+difficulty = 2
 
-potion_health = random.randint(25, 50)
+potion_health = int(random.randint(25, 50) / difficulty)
 
 health = health + potion_health
 print(health)
